7fd339c2c1c273a3a13fcea76dd395b89a904fa0
Ant3ney <46980442+Ant3ney@users.noreply.github.com>
2020-10-01 00:40:34 -0700

    Add availableDays to api

---
+++ gameLogic/templetesAndPresets/availableDays.js
@@ -0,0 +1,32 @@
+var availableDays = {
+    monday: {
+        day: true,
+        night: true
+    },
+    tuesday: {
+        day: true,
+        night: true
+    },
+    wednesday: {
+        day: true,
+        night: true
+    },
+    thursday: {
+        day: true,
+        night: true
+    },
+    firday: {
+        day: true,
+        night: true
+    },
+    saturday: {
+        day: true,
+        night: true
+    },
+    sunday: {
+        day: true,
+        night: true
+    }
+}
+
+module.exports = availableDays;
--- gameLogic/templetesAndPresets/initialData.js
+++ gameLogic/templetesAndPresets/initialData.js
@@ -1,11 +1,13 @@
 var testCharacter = require("./testCharacters");
 var assemblySchedule = require("../assemblySchedule");
+var availableDays = require("./availableDays");
 
 //In future this data should be derived from user mongo model
 var data = {
     testCharacters: testCharacter,
     shiftStructure: assemblySchedule(testCharacter),
-
+    availableDays: availableDays,
+    
     //for now
     initialChapter: 0
 }
