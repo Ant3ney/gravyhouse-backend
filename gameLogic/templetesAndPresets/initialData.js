0eaf22185916d356a9325c7e305da46b3e901fc2
Ant3ney <46980442+Ant3ney@users.noreply.github.com>
2020-10-06 07:36:55 -0700

    Fix testing route so that the shifts are acturaly random

--- gameLogic/templetesAndPresets/initialData.js
+++ gameLogic/templetesAndPresets/initialData.js
@@ -2,15 +2,16 @@ var testCharacter = require("./testCharacters");
 var assemblySchedule = require("../assemblySchedule");
 var availableDays = require("./availableDays");
 
-//In future this data should be derived from user mongo model
-var data = {
-    testCharacters: testCharacter,
-    shiftStructure: assemblySchedule(testCharacter),
-    availableDays: availableDays,
+module.exports = () => {
 
-    //for now
-    initialChapter: 0,
-    currentDay: "monday"
-}
-
-module.exports = data;
+    //In future this data should be derived from user mongo model
+    return {
+        testCharacters: testCharacter,
+        shiftStructure: assemblySchedule(testCharacter),
+        availableDays: availableDays,
+    
+        //for now
+        initialChapter: 0,
+        currentDay: "monday"
+    }
+};
--- routs/testing.js
+++ routs/testing.js
@@ -4,7 +4,7 @@ var initialData = require("../gameLogic/templetesAndPresets/initialData");
 var assembleSchedule = require("../gameLogic/assemblySchedule");
 
 router.get("/characters", (req, res) => {
-    res.json(initialData);
+    res.json(initialData());
 });
 router.get("/schedule", (req, res) => {
     res.json(assembleSchedule(require("../gameLogic/templetesAndPresets/testCharacters")));
