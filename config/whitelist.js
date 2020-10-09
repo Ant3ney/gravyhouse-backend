b2377c2114b55beeb04e9eafbc54a29fb16af08b
Ant3ney <46980442+Ant3ney@users.noreply.github.com>
2020-10-08 20:27:27 -0700

    Add draft website to whitelinst

--- config/whitelist.js
+++ config/whitelist.js
@@ -2,7 +2,8 @@
 
 var whitelist = [
     "http://localhost:19006",
-	"https://www.google.com"
+    "https://www.google.com",
+    "https://5f7fd7c0c0eea15730d43b99--coco-game.netlify.app"
 ]
 
 module.exports = whitelist;
