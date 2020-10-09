096bcd0c42b8cafb1dd90cb2b93090e44a07621d
Ant3ney <46980442+Ant3ney@users.noreply.github.com>
2020-10-08 20:37:44 -0700

    Add front end production website to whitelist

--- config/whitelist.js
+++ config/whitelist.js
@@ -3,7 +3,8 @@
 var whitelist = [
     "http://localhost:19006",
     "https://www.google.com",
-    "https://5f7fd7c0c0eea15730d43b99--coco-game.netlify.app"
+    "https://5f7fd7c0c0eea15730d43b99--coco-game.netlify.app",
+    "https://coco-game.netlify.app"
 ]
 
 module.exports = whitelist;
